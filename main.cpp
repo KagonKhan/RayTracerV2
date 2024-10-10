fcc5eac1dc6ca4fad615d2c4efd44b3d3492a8d3
KagonKhan <s.szynk98@gmail.com>
2024-10-10 18:42:10 +0200

    setup

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <print>
+#include <ranges>
+#include <source_location>
+
+int main(){
+    std::print(std::cout, std::source_location::current().function_name());
+    
+}
